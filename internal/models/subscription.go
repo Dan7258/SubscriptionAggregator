434df8945be9cf42710d525223de97f38b4a8475
Даниил Итальев <dan7258@MacBook-Pro-Daniil.local>
2025-09-05 11:57:08 +0300

    develop crud

--- internal/models/subscription.go
+++ internal/models/subscription.go
@@ -12,3 +12,25 @@ type Subscription struct {
 	UserID      uuid.UUID `json:"user_id"`
 	StartDate   time.Time `json:"start_date"`
 }
+
+func (db PostgresDatabase) CreateSubscription(subscription Subscription) error {
+	err := db.Conn.Create(&subscription).Error
+	return err
+}
+
+func (db PostgresDatabase) GetSubscriptionByID(id uint) (Subscription, error) {
+	var subscription Subscription
+	err := db.Conn.First(&subscription, id).Error
+	return subscription, err
+}
+
+func (db PostgresDatabase) UpdateSubscriptionByID(subscription Subscription) error {
+	err := db.Conn.Save(&subscription).Error
+	return err
+}
+
+func (db PostgresDatabase) GetSubscriptions() ([]Subscription, error) {
+	var subscriptions []Subscription
+	err := db.Conn.Find(&subscriptions).Error
+	return subscriptions, err
+}
